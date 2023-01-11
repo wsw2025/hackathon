130dc3875d235312002267f1c1522d7feda7fe6a
Your Name <you@example.com>
2023-01-11 19:37:19 +0800

    fix top.html

---
+++ src/main/java/com/xin/aoc/config/LocaleConfig.java
@@ -0,0 +1,29 @@
+package com.xin.aoc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
---
+++ src/main/java/com/xin/aoc/controller/LocaleController.java
@@ -0,0 +1,21 @@
+package com.xin.aoc.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Locale;
+
+@Controller
+public class LocaleController {
+    static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping(value = "/locale")
+    public String localeHandler(HttpServletRequest request, Locale locale) {
+        String lastUrl = request.getHeader("referer");
+        lastUrl = lastUrl != null ? lastUrl : "/";
+        return "redirect:" + lastUrl;
+    }
+}
