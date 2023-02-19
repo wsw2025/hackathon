926c190d6aabf626fb5572c16031c2abb1fd5a1a
Cicicai379 <caiyuxin379@gmail.com>
2023-02-19 18:44:17 +0800

    newest

--- src/main/java/com/xin/aoc/WebApp.java
+++ src/main/java/com/xin/aoc/WebApp.java
@@ -3,11 +3,16 @@ package com.xin.aoc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @SpringBootApplication
 @MapperScan("com.xin.aoc.mapper")
 public class WebApp {
+
     public static void main(String[] args) {
-        SpringApplication.run(WebApp.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(WebApp.class, args);
+        ThymeleafViewResolver resolver = (ThymeleafViewResolver) context.getBean("thymeleafViewResolver");
+        resolver.setRedirectHttp10Compatible(false);
     }
 }
