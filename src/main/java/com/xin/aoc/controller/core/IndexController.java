31829ccb0fbbded9cf8cd6a41704cdab7888b75a
wsw2025 <shuwenw2025@student.cis.edu.hk>
2024-02-19 21:55:37 +0800

    1st change

--- src/main/java/com/xin/aoc/controller/core/IndexController.java
+++ src/main/java/com/xin/aoc/controller/core/IndexController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping(value = "/")
-    public String index(HttpServletRequest request) {
-        return "index";
-    }
-    @GetMapping(value = "/hackathon")
     public String hackathon(HttpServletRequest request) {
         return "hackathon";
     }
+//    @GetMapping(value = "/hackathon")
+//    public String hackathon(HttpServletRequest request) {
+//        return "hackathon";
+//    }
 
     @GetMapping(value = "/faq")
     public String faq(HttpServletRequest request) {
@@ -32,21 +32,6 @@ public class IndexController {
     public String eligibility(HttpServletRequest request) {
         return "eligibility";
     }
-
-    @GetMapping(value = "/cca")
-    public String cca(HttpServletRequest request) {
-        return "CCA";
-    }
-    @GetMapping(value = "/resources")
-    public String resources( HttpServletRequest request) {
-        return "resources";
-    }
-
-    @GetMapping(value = "/team")
-    public String team(HttpServletRequest request) {
-        return "team";
-    }
-
 
 
 
